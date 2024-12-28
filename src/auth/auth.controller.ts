cbf52464ac4f2177b8dabfbc0badf05144393b5e
Anas <anas.9630962@gmail.com>
2024-12-28 08:56:32 +0300

    edit validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,56 +1,38 @@
 /* eslint-disable prettier/prettier */
 import { RegisterDto } from './dto/register.dto';
-import {
-    Controller,
-    Post,
-    Body,
-} from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) { }
+  constructor(private readonly authService: AuthService) {}
 
-    @Post('register')
-    async register(@Body() createUserDto: RegisterDto) {
-        try {
-            const response = await this.authService.register(createUserDto);
-            return {
-                status: 201,
-                ok: true,
-                response,
-            };
-        } catch (error) {
-            if (error.status === 400) {
-                return {
-                    status: 400,
-                    ok: false,
-                    validation: error.response.validation,
-                };
-            }
-            throw error;
-        }
+  @Post('register')
+  async register(@Body() createUserDto: RegisterDto) {
+    try {
+      const response = await this.authService.register(createUserDto);
+      return {
+        status: 201,
+        ok: true,
+        response,
+      };
+    } catch (error) {
+      throw error;
     }
+  }
 
-    @Post('login')
-    async login(@Body() loginDto: LoginDto) {
-        try {
-            const response = await this.authService.login(loginDto);
-            return {
-                status: 200,
-                ok: true,
-                response,
-            };
-        } catch (error) {
-            if (error.status === 400) {
-                return {
-                    status: 400,
-                    ok: false,
-                    validation: error.response.validation,
-                };
-            }
-            throw error;
-        }
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    try {
+      const response = await this.authService.login(loginDto);
+      return {
+        status: 200,
+        ok: true,
+        response,
+      };
+    } catch (error) {
+      throw error;
     }
+  }
 }
