9c8f077b9d49852b7b656101b56cb7ce8372e30d
leen-alkurdi <lolo.exo88@gmail.com>
2025-01-02 15:53:39 +0300

    logo

--- src/tiktok-campaign/tiktok-campaign.controller.ts
+++ src/tiktok-campaign/tiktok-campaign.controller.ts
@@ -58,7 +58,8 @@ export class TiktokCampaignController {
   @UseInterceptors(
     FileFieldsInterceptor([
       { name: 'videoFile', maxCount: 1 },
-      { name: 'imageFiles', maxCount: 2 },
+      { name: 'logoFile', maxCount: 1 },
+      { name: 'coverFile', maxCount: 1 },
     ]),
   )
   async FeedAd(
@@ -67,7 +68,8 @@ export class TiktokCampaignController {
     @UploadedFiles()
     files: {
       videoFile?: Express.Multer.File[];
-      imageFiles?: Express.Multer.File[];
+      logoFile?: Express.Multer.File[];
+      coverFile?:Express.Multer.File[];
     },
   ) {
     const {
@@ -90,8 +92,8 @@ export class TiktokCampaignController {
       locationIds: rawLocationIds,
       interestCategoryIds,
       operatingSystems,
-      devicePriceRanges,
-      deviceModelIds,
+      // devicePriceRanges,
+      // deviceModelIds,
       scheduleEndTime,
     } = body;
     const locationIds = Array.isArray(rawLocationIds)
@@ -104,7 +106,6 @@ export class TiktokCampaignController {
               .map((item) => item.trim().replace(/"/g, '')),
           ),
         ];
-
     if (
       !accessToken ||
       !advertiserId ||
@@ -123,8 +124,8 @@ export class TiktokCampaignController {
     if (
       !files.videoFile ||
       files.videoFile.length === 0 ||
-      !files.imageFiles ||
-      files.imageFiles.length === 0
+      !files.logoFile ||
+      files.logoFile.length === 0
     ) {
       throw new HttpException(
         'Both video and image files are required.',
@@ -143,8 +144,8 @@ export class TiktokCampaignController {
     const processedDayparting = this.convertDaypartingToString(parsedDayparting);
   
     const videoFile = files.videoFile[0];
-    const coverFile = files.imageFiles[1];
-    const logoFile=files.imageFiles[0]
+    const coverFile = files.coverFile[0];
+    const logoFile=files.logoFile[0]
     console.log(req.user);
     const result = await this.campaignService.CreateFeed(
       req.user.id,
@@ -168,8 +169,8 @@ export class TiktokCampaignController {
       locationIds,
       interestCategoryIds,
       operatingSystems,
-      devicePriceRanges,
-      deviceModelIds,
+      // devicePriceRanges,
+      // deviceModelIds,
       videoFile,
       coverFile,
       logoFile,
