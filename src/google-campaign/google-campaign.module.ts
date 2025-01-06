45eb5c94d7bed203f1d3a1b957e3fe79b6dbda26
Alaa Al-Midani <alaaalmedane@gmail.com>
2025-01-06 23:08:29 +0300

    google campaign

--- src/google-campaign/google-campaign.module.ts
+++ src/google-campaign/google-campaign.module.ts
@@ -1,9 +1,9 @@
-// import { Module } from '@nestjs/common';
-// import { GoogleCampaignController } from './google-campaign.controller';
-// import { GoogleCampaignService } from './google-campaign.service';
+import { Module } from '@nestjs/common';
+import { GoogleCampaignController } from './google-campaign.controller';
+import { GoogleCampaignService } from './google-campaign.service';
 
-// @Module({
-//   controllers: [GoogleCampaignController],
-//   providers: [GoogleCampaignService],
-// })
-// export class GoogleCampaignModule {}
+@Module({
+  controllers: [GoogleCampaignController],
+  providers: [GoogleCampaignService],
+})
+export class GoogleCampaignModule {}
