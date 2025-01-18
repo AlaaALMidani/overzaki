150baf3cc14b8817d1961a655f13b93564d8ed06
leen-alkurdi <lolo.exo88@gmail.com>
2025-01-18 17:16:06 +0300

    Up

--- src/snapchat-campaign/snapchat-campaign.service.ts
+++ src/snapchat-campaign/snapchat-campaign.service.ts
@@ -210,7 +210,7 @@ export class SnapchatCampaignService {
             ad_account_id: adAccountId,
             status: 'PAUSED',
             start_time: startTime,
-            objective,
+            objective_v2:objective,
           },
         ],
       };
