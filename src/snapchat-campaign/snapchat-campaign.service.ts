7d16f5947c2866ec12e8d5873624d00545a4f617
leen-alkurdi <lolo.exo88@gmail.com>
2025-01-22 17:07:24 +0300

    t

--- src/snapchat-campaign/snapchat-campaign.service.ts
+++ src/snapchat-campaign/snapchat-campaign.service.ts
@@ -907,7 +907,6 @@ export class SnapchatCampaignService {
     productUrls: string[],
     callToAction: string,
     mainFile: string,
-    mainFile: string,
     product1: string,
     product2: string,
     product3: string,
